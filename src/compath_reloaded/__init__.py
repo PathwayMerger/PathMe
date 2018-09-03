709a9611d3ecae4a8c88e5514bade24e0c77420d
10mubeen <mubeensarah@hotmail.com>
2018-09-03 16:58:12 +0200

    Update __init__.py
    update init

--- src/compath_reloaded/__init__.py
+++ src/compath_reloaded/__init__.py
@@ -1,6 +1,66 @@
 # -*- coding: utf-8 -*-
 
-"""A systematic comparison across pathway databases."""
+"""ComPath-Reloaded
+
+Compath-Reloaded facilitates a systematic comparison across three of the major pathway databases.
+
+The primary goal of this package is to convert KEGG, Reactome, and WikiPathways to Biological Expression Language (BEL).
+ComPath-Reloaded is the continuation of the ComPath web application which is aimed at exploring, analyzing, and curating pathway
+knowledge in a more simplistic, gene-centric view. This approach involves converting all pathways to BEL as a pivotal integration schema and evaluating consensus and gaps in pathway knowledge. Additionally,
+ComPath-Reloaded is complemented with PathMe, a web application that enables the exploration of all the pathways from these
+resources using the mappings curated from ComPath.
+
+Installation
+------------
+1. ``compath-reloaded`` can be installed with the following commmands:
+
+.. code-block:: sh
+
+    python3 -m pip install git+https://github.com/ComPath/ComPath-Reloaded.git@master
+
+2. or in editable mode with:
+
+.. code-block:: sh
+
+    git clone https://github.com/ComPath/ComPath-Reloaded.git
+
+.. code-block:: sh
+
+    cd compath-reloaded
+
+.. code-block:: sh
+
+    python3 -m pip install -e .
+
+How to use
+----------
+
+1. Download content
+
+ComPath Reloaded first requires to download the raw files from the original pathway databases.
+
+.. code-block:: python
+
+    python3 -m compath_reloaded download
+
+2. Generate BEL Graphs
+
+.. code-block:: python
+
+    python3 -m compath_reloaded populate
+
+Alternatively, you can do any of these two steps for a particular database by the following command:
+
+.. code-block:: python
+
+    python3 -m compath_reloaded database_name action
+
+Example:
+
+.. code-block:: python
+
+    python3 -m compath_reloaded kegg download
+"""
 
 import logging
 
@@ -12,8 +72,8 @@ __title__ = 'compath_reloaded'
 __description__ = "A systematic comparison across pathway databases"
 __url__ = 'https://gitlab.scai.fraunhofer.de/sarah.mubeen/ComPath-Reloaded'
 
-__author__ = 'Sarah Mubeen and Daniel Domingo-Fernández'
+__author__ = 'Sarah Mubeen, Daniel Domingo-Fernández & Josep Marín-Llaó'
 __email__ = 'daniel.domingo.fernandez@scai.fraunhofer.de'
 
 __license__ = 'MIT License'
-__copyright__ = 'Copyright (c) 2018 Sarah Mubeen and Daniel Domingo-Fernández'
+__copyright__ = 'Copyright (c) 2018 Sarah Mubeen, Daniel Domingo-Fernández & Josep Marín-Llaó'
