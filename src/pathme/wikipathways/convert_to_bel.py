7446117c6f62a1dc9fc8b8cabf591c0cf942e1bf
ddomingof <danieldomingofernandez@hotmail.com>
2018-09-14 09:30:51 +0200

    Refactor

--- src/pathme/wikipathways/convert_to_bel.py
+++ src/pathme/wikipathways/convert_to_bel.py
@@ -12,7 +12,7 @@ from pathme.utils import parse_id_uri
 log = logging.getLogger(__name__)
 
 
-def convert_to_bel(nodes: Dict[str, Dict], interactions: List[Tuple[str, str, Dict]], pathway_info) -> BELGraph:
+def wikipathways_to_bel(nodes: Dict[str, Dict], interactions: List[Tuple[str, str, Dict]], pathway_info) -> BELGraph:
     graph = BELGraph(
         name=pathway_info['title'],
         version='1.0.0',
