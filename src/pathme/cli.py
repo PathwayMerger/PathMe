a923d873dc2622a711d8bc548bd78efe12d0472e
ddomingof <danieldomingofernandez@hotmail.com>
2019-01-14 14:58:22 +0100

    Modify message kegg parser

--- src/pathme/cli.py
+++ src/pathme/cli.py
@@ -47,7 +47,7 @@ def download(connection):
     if click.confirm(
             'You are about to download KGML files from KEGG.\n'
             'Please make sure you have read KEGG license (see: https://www.kegg.jp/kegg/rest/).'
-            ' These files cannot be distributed and their use must be exclusively academic.\n'
+            ' These files cannot be distributed and their use must be exclusively with academic purposes.\n'
             'We (PathMe developers) are not responsible for the end use of this data.\n'
     ):
         click.echo('You have read and accepted the conditions stated above.\n')
