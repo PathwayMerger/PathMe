48ab6399e40104826f3a29d89265ddcf93177deb
ddomingof <danieldomingofernandez@hotmail.com>
2019-01-15 15:12:51 +0100

    Add wp tests

--- tests/test_wikipathways/test_wikipathways.py
+++ tests/test_wikipathways/test_wikipathways.py
@@ -9,18 +9,18 @@ from pybel_tools.summary.edge_summary import count_relations
 from pathme.utils import parse_rdf
 from pathme.wikipathways.rdf_sparql import wikipathways_to_bel, _get_nodes, _get_interactions
 from pybel import BELGraph
-from tests.constants import WP22, WP2359
+from tests.constants import WikipathwaysTest, WP22, WP2359, WP706, WP1871, WP2799
 
 
-class TestWikipathways(unittest.TestCase):
+class TestWikipathways(WikipathwaysTest):
     """Tests for dealing with the WikiPathways sub-module."""
 
     def test_get_nodes(self):
         """Test get nodes from RDF pathways files."""
         wp22_rdf_graph = parse_rdf(WP22)
-        wp706_rdf_graph = parse_rdf(WP22)
-        wp1871_rdf_graph = parse_rdf(WP22)
-        wp2799_rdf_graph = parse_rdf(WP22)
+        wp706_rdf_graph = parse_rdf(WP706)
+        wp1871_rdf_graph = parse_rdf(WP1871)
+        wp2799_rdf_graph = parse_rdf(WP2799)
 
         nodes_wp22 = _get_nodes(wp22_rdf_graph)
         nodes_wp706 = _get_nodes(wp706_rdf_graph)
@@ -28,30 +28,30 @@ class TestWikipathways(unittest.TestCase):
         nodes_wp2799 = _get_nodes(wp2799_rdf_graph)
 
         self.assertEqual(len(nodes_wp22), 17)
-        self.assertEqual(len(nodes_wp706), 181)
-        self.assertEqual(len(nodes_wp1871), 102)
-        self.assertEqual(len(nodes_wp2799), 139)
+        self.assertEqual(len(nodes_wp706), 186)
+        self.assertEqual(len(nodes_wp1871), 115)
+        self.assertEqual(len(nodes_wp2799), 141)
 
     def test_get_interactions(self):
         """Test get nodes from RDF pathways files."""
         wp22_rdf_graph = parse_rdf(WP22)
-        wp706_rdf_graph = parse_rdf(WP22)
-        wp1871_rdf_graph = parse_rdf(WP22)
-        wp2799_rdf_graph = parse_rdf(WP22)
+        wp706_rdf_graph = parse_rdf(WP706)
+        wp1871_rdf_graph = parse_rdf(WP1871)
+        wp2799_rdf_graph = parse_rdf(WP2799)
 
         nodes_wp22 = _get_interactions(wp22_rdf_graph)
         nodes_wp706 = _get_interactions(wp706_rdf_graph)
         nodes_wp1871 = _get_interactions(wp1871_rdf_graph)
         nodes_wp2799 = _get_interactions(wp2799_rdf_graph)
 
-        self.assertEqual(len(nodes_wp22), 17)
+        self.assertEqual(len(nodes_wp22), 10)
         self.assertEqual(len(nodes_wp706), 44)
-        self.assertEqual(len(nodes_wp1871), 102)
-        self.assertEqual(len(nodes_wp2799), 38)
+        self.assertEqual(len(nodes_wp1871), 51)
+        self.assertEqual(len(nodes_wp2799), 28)
 
     def test_wp_association_bp(self):
         """Test connect bp with protein."""
-        test_graph = wikipathways_to_bel(WP2359)
+        test_graph = wikipathways_to_bel(WP2359, self.hgnc_manager)
 
         self.assertEqual(type(test_graph), BELGraph, msg='Error with graph type')
 
@@ -61,7 +61,7 @@ class TestWikipathways(unittest.TestCase):
 
     def test_wp_22_to_bel(self):
         """Test 22."""
-        test_graph = wikipathways_to_bel(WP22)
+        test_graph = wikipathways_to_bel(WP22, self.hgnc_manager)
 
         self.assertEqual(type(test_graph), BELGraph, msg='Error with graph type')
 
