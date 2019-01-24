f293e5ecd57bd843aa9995a40eed8dd4b595274d
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2019-01-24 19:32:13 +0100

    Bump version: 0.0.9-dev → 0.0.9

--- src/pathme/__init__.py
+++ src/pathme/__init__.py
@@ -65,7 +65,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
-__version__ = '0.0.9-dev'
+__version__ = '0.0.9'
 
 __title__ = 'pathme'
 __description__ = "Harmonizing pathway databases using Biological Expression Language (BEL)"
