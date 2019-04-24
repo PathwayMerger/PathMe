f055e4cfcd444c3da0e4876cc6355071b3771393
jmarinllao <josepmarinllao@gmail.com>
2019-04-24 14:56:45 +0200

    left_full_data_join PyBEL wrappers moved to utils.

--- src/pathme/export_utils.py
+++ src/pathme/export_utils.py
@@ -6,11 +6,15 @@ import logging
 import os
 from typing import List
 
-from pathme.constants import KEGG, REACTOME, WIKIPATHWAYS
+import networkx as nx
+import pybel
+from pathme.constants import KEGG, REACTOME, WIKIPATHWAYS, PATHME_DIR
 from pathme.normalize_names import normalize_graph_names
 from pathme.pybel_utils import flatten_complex_nodes
 from pybel import BELGraph, from_pickle
-from pathme.utils import union
+from pybel.struct.utils import update_metadata
+from pybel.constants import RELATION
+
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
@@ -48,6 +52,58 @@ def get_all_pickles(kegg_path, reactome_path, wikipathways_path):
 
     return kegg_pickles, reactome_pickles, wp_pickles
 
+
+def left_full_data_join(g, h) -> None:
+    """Wrapper around PyBEL's left_full_join to merge node data.
+
+    :param pybel.BELGraph g: A BEL graph
+    :param pybel.BELGraph h: A BEL graph
+    """
+    for node, data in h.nodes(data=True):
+        if node in g:
+            if 'database' in data and 'database' in g.nodes[node]:
+                g.nodes[node]['database'].update(data['database'])
+        else:
+            g.add_node(node, **data)
+
+    g.add_edges_from(
+        (u, v, key, data)
+        for u, v, key, data in h.edges(keys=True, data=True)
+        if u not in g or v not in g[u] or key not in g[u][v]
+    )
+
+    update_metadata(h, g)
+
+    g.warnings.extend(h.warnings)
+
+
+def union_data(graphs):
+    """Wrapper around PyBEL's union to instate left_full_data_join function.
+
+    Assumes iterator is longer than 2, but not infinite.
+
+    :param iter[BELGraph] graphs: An iterator over BEL graphs. Can't be infinite.
+    :return: A merged graph
+    :rtype: BELGraph
+
+    Example usage:
+    """
+    graphs = tuple(graphs)
+
+    n_graphs = len(graphs)
+
+    if n_graphs == 0:
+        raise ValueError('no graphs given')
+
+    if n_graphs == 1:
+        return graphs[0]
+
+    target = graphs[0].copy()
+
+    for graph in graphs[1:]:
+        left_full_data_join(target, graph)
+
+    return target
 
 def get_universe_graph(
         kegg_path: str, reactome_path: str, wikipathways_path: str,
@@ -99,7 +155,7 @@ def get_universe_graph(
                 flatten_complex_nodes(graph)
 
             if normalize_names:
-                normalize_graph_names(graph, WIKIPATHWAYS)
+                normalize_grget_set_databaseaph_names(graph, WIKIPATHWAYS)
 
             set_graph_resource(graph, 'wikipathways')
 
@@ -111,8 +167,35 @@ def get_universe_graph(
 
     logger.info('Merging all into a hairball...')
 
-    return union(universe_list)
+    return union_data(universe_list)
 
+
+def munge_node_attribute(node, attribute='name'):
+    if node.get(attribute) == None:
+        return str(node)
+    else:
+        return node.get(attribute)
+
+
+def to_gml(graph: pybel.BELGraph, path: str = PATHME_DIR) -> None:
+    """Write this graph to GML  file using :func:`networkx.write_gml`.
+    """
+    rv = nx.MultiDiGraph()
+
+    for node in graph:
+        rv.add_node(munge_node_attribute(node, 'name'), namespace=str(node.get('namespace')),
+                    function=node.get('function'))
+
+    for u, v, key, edge_data in graph.edges(data=True, keys=True):
+        rv.add_edge(
+            munge_node_attribute(u),
+            munge_node_attribute(v),
+            interaction=str(edge_data[RELATION]),
+            bel=str(edge_data),
+            key=str(key),
+        )
+
+    nx.write_gml(rv, path)
 
 def get_files_in_folder(path: str) -> List[str]:
     """Return the files in a given folder.
