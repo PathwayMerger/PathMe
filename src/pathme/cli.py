0048674c69c9b8f56a334e15283c135f19b92f59
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2020-04-02 08:53:48 +0200

    Fix flake8

--- src/pathme/cli.py
+++ src/pathme/cli.py
@@ -15,6 +15,7 @@ from pybel.struct.summary import count_functions
 import pathme.kegg.cli
 import pathme.reactome.cli
 import pathme.wikipathways.cli
+
 from .constants import CX_DIR, KEGG_BEL, PPI_DIR, REACTOME_BEL, SPIA_DIR, UNIVERSE_DIR, WIKIPATHWAYS_BEL
 from .export_utils import export_helper, get_universe_graph, iterate_universe_graphs
 
